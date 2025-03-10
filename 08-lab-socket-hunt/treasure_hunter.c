4f9852fd38a3e1f90e9442bf7eb8178709ff362d
Ethan Walch <ewalch112@gmail.com>
2025-03-10 16:42:33 -0600

    Finish Level 3 and part of 4

--- 08-lab-socket-hunt/treasure_hunter.c
+++ 08-lab-socket-hunt/treasure_hunter.c
@@ -76,7 +76,6 @@ int main(int argc, char *argv[]) {
 	memcpy(remote_addr, result->ai_addr, sizeof(struct sockaddr_storage));
 	parse_sockaddr(remote_addr, remote_ip, &remote_port);
 	populate_sockaddr(remote_addr, addr_fam, remote_ip, remote_port);
-	freeaddrinfo(result);
 
 	addr_len = sizeof(struct sockaddr_storage);
 	s = getsockname(sfd, local_addr, &addr_len);
@@ -126,16 +125,72 @@ int main(int argc, char *argv[]) {
 		memcpy(&nonce, &buf2[chunk_length + 4], sizeof(unsigned int));
 		nonce = ntohl(nonce);
 
-		if (op_code == 1) {
-			remote_port = op_param;
-			populate_sockaddr(remote_addr, addr_fam, remote_ip, remote_port);
-		}
-	
 		// printf("%x\n", chunk_length);
 		// printf("%s\n", chunk);
 		// printf("%x\n", op_code);
 		// printf("%x\n", op_param);
 		// printf("%x\n", nonce);
+
+		if (op_code == 1) {
+			remote_port = op_param;
+			populate_sockaddr(remote_addr, addr_fam, remote_ip, remote_port);
+		}
+		else if (op_code == 2) {
+			close(sfd);
+			local_port = op_param;
+			populate_sockaddr(local_addr, addr_fam, NULL, local_port);
+			sfd = socket(result->ai_family, result->ai_socktype, 0);
+			if (bind(sfd, local_addr, sizeof(struct sockaddr_storage)) < 0) {
+				perror("bind()");
+			}
+		}
+		else if (op_code == 3) {
+			struct sockaddr_storage remote_addr_ss_temp;
+			struct sockaddr *remote_addr_temp = (struct sockaddr *)&remote_addr_ss_temp;
+			memcpy(remote_addr_temp, result->ai_addr, sizeof(struct sockaddr_storage));
+			parse_sockaddr(remote_addr_temp, remote_ip, &remote_port);
+			populate_sockaddr(remote_addr_temp, addr_fam, remote_ip, remote_port);
+			unsigned short temp_remote_port;
+			unsigned int sum = 0;
+			for (int i = 0; i < op_param; i++) {
+				unsigned char tempbuf[256];
+				ssize_t nread = recvfrom(sfd, tempbuf, 256, 0, remote_addr_temp, &addr_len);
+				parse_sockaddr(remote_addr_temp, remote_ip, &temp_remote_port);
+				sum += temp_remote_port;
+				tempbuf[nread] = '\0';
+				// printf("Temp port: %x", temp_remote_port);
+				// parse_sockaddr
+				// if (nread < 0) {
+				// 	perror("read");
+				// 	exit(EXIT_FAILURE);
+				// }
+			}
+			nonce = sum;
+		}
+		else if (op_code == 4) {
+			close(sfd);
+			remote_port = op_param;
+			populate_sockaddr(remote_addr, addr_fam, remote_ip, remote_port);
+			if (hints.ai_family == AF_INET) {
+				hints.ai_family = AF_INET6;
+			} 
+			else {
+				hints.ai_family = AF_INET;
+			}
+			char port_string[8];
+			sprintf(port_string, "%d", remote_port);
+			struct addrinfo *new_result;
+			s = getaddrinfo(server, port_string, &hints, &new_result);
+			sfd = socket(new_result->ai_family, new_result->ai_socktype, 0);
+			addr_fam = new_result->ai_family;
+			addr_len = new_result->ai_addrlen;
+			memcpy(remote_addr, new_result->ai_addr, sizeof(struct sockaddr_storage));
+			parse_sockaddr(remote_addr, remote_ip, &remote_port);
+			populate_sockaddr(remote_addr, addr_fam, remote_ip, remote_port);
+			addr_len = sizeof(struct sockaddr_storage);
+			s = getsockname(sfd, local_addr, &addr_len);
+			parse_sockaddr(local_addr, local_ip, &local_port);
+		}
 	
 		unsigned char return_msg[4]; 
 		bzero(return_msg, 4);
